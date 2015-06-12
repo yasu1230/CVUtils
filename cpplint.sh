d9b349cdf3e0c97420c0bf04fb0ac970513d168a
yinoue <yinoue@densan-soft.co.jp>
2015-06-12 15:53:10 +0900

    fixed directries list

--- cpplint.sh
+++ cpplint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find include src tests examples -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp
+find include tests -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp
