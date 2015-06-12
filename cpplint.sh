5cbbb3a61b5128e2cb75347fe3fee8e56024ce2f
yinoue <yinoue@densan-soft.co.jp>
2015-06-12 16:09:18 +0900

    exclude tests directory

--- cpplint.sh
+++ cpplint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find include tests -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp
+find include -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp
