665413806eaa9e31668ed683075a8f55f3132817
yinoue <yinoue@densan-soft.co.jp>
2015-06-25 14:32:56 +0900

    remove filter.

--- cpplint.sh
+++ cpplint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find include -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp --filter=-runtime/references
+find include -regex ".*\.c\|.*\.cpp\|.*\.h\|.*\.hpp" | xargs python cpplint.py --extensions=h,hpp,c,cpp
